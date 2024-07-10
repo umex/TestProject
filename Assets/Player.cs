d06351ced327ff2c2273d9d7405cc1c275cfe5b6
dobrez <dejan.obrez@gmail.com>
2024-07-10 22:08:03 +0200

    init commit

---
+++ Assets/Player.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField] private float moveSpeed = 1;
+    [SerializeField] private float jumpForce = 1;
+
+    [SerializeField] private float xInput;
+
+    private int facinDir = 1;
+    private bool facingRight = true;
+    [SerializeField] private float groundCheckDistance = 1;
+    [SerializeField] private LayerMask whatIsGround = 1;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CollisionChecks();
+        Movement();
+        CheckInput();
+        TurnController();
+        AnimatorControllers();
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        if (isGrounded) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        
+    }
+
+    private void AnimatorControllers() {
+
+        bool isMovin = rb.velocity.x != 0;
+
+        anim.SetBool("isMoving", isMovin);
+    }
+
+    private void TurnCharacter() {
+        facinDir = facinDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0,180,0);
+    }
+
+    private void TurnController() {
+        if (rb.velocity.x > 0 && !facingRight) {
+            TurnCharacter();
+        }
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            TurnCharacter();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, 
+            new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+
+}
